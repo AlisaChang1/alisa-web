640be95167f210c4acc9f36128056352a300a3a1
Alisa Chang <alisa.chang@rogers.com>
2020-04-17 17:16:43 -0400

    Updates

---
+++ precache-manifest.2b398ca32a09e3adc925e62fdc7a1f56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29edecbdad4f6b1b8925dfe161a436ed",
+    "url": "/alisa-web/index.html"
+  },
+  {
+    "revision": "cd79e6b6fe5ca88dc5da",
+    "url": "/alisa-web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e22ea34a18e2a471bdba",
+    "url": "/alisa-web/static/js/2.c82b8699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alisa-web/static/js/2.c82b8699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd79e6b6fe5ca88dc5da",
+    "url": "/alisa-web/static/js/main.1c1a2a6a.chunk.js"
+  },
+  {
+    "revision": "b07abaa6dc4487c920c0",
+    "url": "/alisa-web/static/js/runtime-main.b9b49233.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alisa-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
