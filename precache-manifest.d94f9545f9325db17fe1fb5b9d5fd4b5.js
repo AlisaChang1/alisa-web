cbed5acd8e2e60cb42320eea813d6688dfeaf540
Alisa Chang <alisachang@Alisas-MacBook-Pro.local>
2020-08-01 11:25:46 -0400

    Updates

---
+++ precache-manifest.d94f9545f9325db17fe1fb5b9d5fd4b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023b4d85a4298b36102f7ada684e61f1",
+    "url": "/alisa-web/index.html"
+  },
+  {
+    "revision": "6e802bef102e06a56b15",
+    "url": "/alisa-web/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b184ddc884ec72e2fcff",
+    "url": "/alisa-web/static/js/2.ec39e6b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/alisa-web/static/js/2.ec39e6b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e802bef102e06a56b15",
+    "url": "/alisa-web/static/js/main.40f8bedc.chunk.js"
+  },
+  {
+    "revision": "b07abaa6dc4487c920c0",
+    "url": "/alisa-web/static/js/runtime-main.b9b49233.js"
+  },
+  {
+    "revision": "952bc86873a80cb5d9bc68dfaa4f65a8",
+    "url": "/alisa-web/static/media/SmallLogo.952bc868.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alisa-web/precache-manifest.cb2a8c0971acb609707c2d6aeb7dbcda.js"
+  "/alisa-web/precache-manifest.d94f9545f9325db17fe1fb5b9d5fd4b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alisa-web/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
