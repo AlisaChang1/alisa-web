ba196c61e1e8c9d04d98330975445f7ee5704dfc
Alisa Chang <alisachang@Alisas-MacBook-Pro.local>
2020-08-02 16:52:04 -0400

    AW-38 adds all links to the navbar and supports mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import StickyFooter from "./components/StickyFooter/StickyFooter";
+import isMobile from "./helpers/isMobile";
+import HamburgerMenu from "./components/HamburgerMenu/HamburgerMenu";
+import { Router } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
+      <>
+        {isMobile() ? <HamburgerMenu /> : <NavBar />}
         <StickyFooter />
-      </div>
+      </>
     );
   }
 }
---
+++ src/pages/Coming Soon/ComingSoon.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ComingSoon = () => {};
+
+export default ComingSoon;
