548ac174c98ff66c913d7b7ea3e0f62549f1a62a
Alisa Chang <alisachang@Alisas-MacBook-Pro.local>
2020-08-15 23:25:36 -0400

    AW-47 adds more to the resume page

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,14 +12,21 @@ export const Paragraph2 =
 
 // Profile.tsx (Not made yet)
 export const ProfileHeader = "Profile";
+// Work Experience Section
+export const ResumeParagraph =
+  "I've worked as a co-op student for three jobs where I worked as a full stack developer. I've worked on production sites to help create new features, user experiments, and learned how to work in agile developments.";
 export const WaterlooDescription =
   "I am currently studying computer engineering at the University of Waterloo. UW has given me the opportunity to study alongside some of the brightest students and has given me the opportunity to learn and gain work experience at the same time. I've made some amazing friends here and I've learned so much about myself.";
+export const WaterlooDate = "2018-2023";
 export const VenngageDescription =
-  "I worked at Venngage for my second co-op. This job offered more insight into my desire to learn about the backend and databases. I worked as a full stack developer which gave me the opportunity to work with PHP and SQL databases.";
+  "I worked at Venngage for my second co-op. This job offered more insight into my desire to learn about the backend and databases. I worked as a full stack developer which gave me the opportunity to work with PHP and SQL databases. I also spent a lot of time working on frontend code using React and Javascript.";
+export const VenngageDate = "September-December 2019";
 export const CIBCDescription =
-  "CIBC was my first co-op and first tech job. I worked at CIBC Live Labs which is their innovation lab. This is where I got my first glimpse of frontend and backend web development.";
+  "CIBC was my first co-op and first tech job. I worked at CIBC Live Labs which is CIBC's innovation lab. This is where I got my first glimpse of frontend and backend web development. This is where I started to learn about React, Javascript and Java. I made amazing friends here and it made me really excited to start working in the industry.";
+export const CIBCDate = "January-April 2019";
 export const POFDescription =
-  "My most recent co-op was at Plenty Of Fish where I worked primarily as a React developer. At this point I felt that I had enough experience in this field and was able to learn more niche aspects of frontend web development.";
+  "My most recent co-op was at Plenty Of Fish where I worked primarily as a React developer. At this point I felt that I had enough experience in this field and was able to learn more niche aspects of frontend web development. I learned a lot of new tools such as, Emotion, styled components, material-ui and so much more. It was a great work environment and I worked with people who helped me grow professionally and personally.";
+export const POFDate = "May-August 2020";
 
 // ComingSoon.tsx
 export const ComingSoonTitle = "Coming Soon!";
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -11,6 +11,7 @@ export const Background = createGlobalStyle`
     }
 `;
 
+// WorkExperience.tsx
 export const Header = styled(Container)`
   width: 100%;
   text-align: center;
@@ -45,4 +46,30 @@ export const Image = styled.img`
   height: 90px;
   width: 90px;
   padding-right: 30px;
+  margin: auto;
+`;
+
+export const DateContainer = styled.div`
+  font-family: Arial;
+  color: white;
+  font-weight: bolder;
+  font-size: 17px;
+  padding-bottom: 5px;
+`;
+
+// Skills.tsx
+export const SkillsHeader = styled(Container)`
+  width: 50% !important;
+  text-align: center;
+  background-color: grey;
+  color: pink;
+  font-family: Arial;
+  font-size: 35px;
+  margin: 80px 0px 60px 0px;
+`;
+
+export const ParagraphContainer = styled(Container)`
+  font-size: 24px;
+  font-family: cosmic-sans;
+  text-align: center;
 `;
