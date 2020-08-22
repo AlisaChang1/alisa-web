6e4e464bce660e6a4ea924ed587873859fc8d78a
Alisa Chang <alisachang@Alisas-MacBook-Pro.local>
2020-08-22 17:00:40 -0400

    AW-25 adds styling for life coaching page

--- src/pages/Life Coaching/styles.ts
+++ src/pages/Life Coaching/styles.ts
@@ -10,3 +10,36 @@ export const Background = createGlobalStyle`
       background-attachment: fixed;
     }
 `;
+
+export const Header = styled(Container)`
+  width: 100%;
+  text-align: center;
+  background-color: pink;
+  color: white;
+  font-family: Arial-black;
+  font-size: 40px;
+  margin: 80px 0px 60px 0px;
+`;
+
+export const IntroContainer = styled(Container)`
+  background-color: grey;
+  height: min-content;
+  width: 90% !important;
+`;
+
+export const IntroText = styled.div`
+  color: white;
+  font-family: arial;
+  font-size: 20px;
+  text-align: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  line-height: 1.4;
+`;
+
+export const StyledLink = styled.a`
+  color: pink;
+  :hover {
+    color: black;
+  }
+`;
